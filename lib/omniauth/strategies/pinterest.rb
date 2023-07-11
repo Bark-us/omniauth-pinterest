62e1695fdeb9f0eee9ff19308388a29f9b1c9e2a
Brandon Hilkert <brandonhilkert@gmail.com>
2023-07-11 12:30:49 -0700

    Update for API v5

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -1,26 +1,22 @@
 require 'omniauth-oauth2'
+require 'base64'
 
 module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site          => 'https://api.pinterest.com/',
-        :authorize_url => 'https://www.pinterest.com/oauth/',
-        :token_url     => 'https://api.pinterest.com/v3/oauth/access_token/'
+        site:          "https://api.pinterest.com/",
+        authorize_url: "https://www.pinterest.com/oauth",
+        token_url:     "https://api.pinterest.com/v5/oauth/token",
+        auth_scheme:   :basic_auth,
       }
 
-      def request_phase
-        options[:scope]         ||= 'read_users'
-        options[:response_type] ||= 'code'
-        super
-      end
-
-      uid { raw_info['id'] }
+      uid { raw_info["id"] }
 
       info { raw_info }
 
       def raw_info
-        @raw_info ||= access_token.get('/v3/users/me/').parsed['data']
+        @raw_info ||= access_token.get("/v5/user_account").parsed
       end
 
       def ssl?
@@ -29,9 +25,9 @@ module OmniAuth
 
       private
 
-      def callback_url
-        options[:redirect_uri] || (full_host + script_name + callback_path)
-      end
+        def callback_url
+          options[:redirect_uri] || (full_host + script_name + callback_path)
+        end
     end
   end
 end
